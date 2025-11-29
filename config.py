57d6337a05f0990bb873335c2af69cb528622dfb
Aliceee275 <gaut.alice@free.fr>
2025-11-29 18:30:20 +0100

    nouvelles figures

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ import os
 
 #%%#chemins d'accès pour chaque personne
 rootpath_alice="C://Users//gauta//OneDrive//Documents//GitHub//fNIRS_projetM2"
-<<<<<<< HEAD
+
 rootpath_marina="C:/Users/marin/Documents/GitHub/fNIRS_projetM2"
-=======
+
 rootpath_imen ="C:/Users/imen4/Documents/fNIRS_projetM2"
->>>>>>> 2ffb539378a23d06a3ae17799c1ae0d1902e0d8b
+
 #ajouter le chemin d'accès du fichier Projet_fNIRS
 
 
--- sanstitre1.py
+++ sanstitre1.py
@@ -1,142 +1,193 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Nov 29 11:42:24 2025
+Created on Fri Nov 28 23:35:19 2025
 
-@author: marin
+@author: gauta
 """
 
-
-import mne
-import os
+#%% Import librairiesimport os
 import config as cfg
 import glob
+import os
+
+# Import common libraries
 from collections import defaultdict
-import matplotlib.pyplot as plt
-from mne.viz import plot_compare_evokeds
-from pprint import pprint
-import pandas as pd
 from copy import deepcopy
+from itertools import compress
+from pprint import pprint
+
+# Import Plotting Library
+import matplotlib.pyplot as plt
+import pandas as pd
 import seaborn as sns
 
-def individual_analysis(file_path):
+# Import StatsModels
+import statsmodels.formula.api as smf
+from mne import Epochs, events_from_annotations, set_log_level
+from mne.preprocessing.nirs import (
+    beer_lambert_law,
+    optical_density,
+    scalp_coupling_index,
+    temporal_derivative_distribution_repair,
+)
 
-    #on ouvre le fichier
-    raw = mne.io.read_raw_nirx(file_path, verbose=False)
+# Import MNE processing
+from mne.viz import plot_compare_evokeds
 
-    #renomme les triggers
-    raw.annotations.rename({'1.0': 'Hard', '2.0': 'Easy'})
+# Import MNE-BIDS processing
+import mne
 
-    #on convertit l'intensité en densité optique
-    raw_od = mne.preprocessing.nirs.optical_density(raw)
+# Import MNE-NIRS processing
+from mne_nirs.channels import get_long_channels, picks_pair_to_idx
+from mne_nirs.datasets import fnirs_motor_group
+from mne_nirs.signal_enhancement import enhance_negative_correlation
 
-    #sous-échantillonage à 2Hz
-    raw_od = raw_od.resample(2)
+# Set general parameters
+set_log_level("WARNING")  # Don't show info, as it is repetitive for many subjects
 
-    #correction des artefacts (TDDR)
-    raw_tddr = mne.preprocessing.nirs.temporal_derivative_distribution_repair(raw_od)
+#%%define the analysis function
 
-    #tansformation en concentration
-    raw_c = mne.preprocessing.nirs.beer_lambert_law(raw_tddr)
+def individual_analysis(path):
+    # load the Nirx data
+    raw_intensity= mne.io.read_raw_nirx(path, verbose=False)
+    raw_intensity = get_long_channels(raw_intensity, min_dist=0.01)
+    
+    #rename trigger 1 and 2 into 'Hard' and 'Easy'
+    #events,event_ids= mne.events_from_annotations(raw_intensity)
+    raw_intensity.annotations.rename({'1.0': 'Hard', '2.0':'Easy'})
 
-    #filtre Bandpass
-    raw_filtre = raw_c.filter(0.02, 0.3, verbose=False)
+    # Convert signal to optical density and determine bad channels
+    raw_od = optical_density(raw_intensity)
+    sci = scalp_coupling_index(raw_od, h_freq=1.35, h_trans_bandwidth=0.1)
+    raw_od.info["bads"] = list(compress(raw_od.ch_names, sci < 0.5))
+    raw_od.interpolate_bads()
 
-    #Applique le nouveau noms aux données filtrées etc.
-    event_id = {'Hard': 1, 'Easy': 2}
-    events, event_ids = mne.events_from_annotations(raw_filtre, event_id=event_id)
+    # Down sample to 2 Hz
+    raw_od.resample(2)
+    
+    #apply movement correction (TDDR)
+    raw_od = temporal_derivative_distribution_repair(raw_od)
 
-    #Analyse 
-    epochs = mne.Epochs(
-        raw_filtre,
-        events,
-        event_id=event_id,
-        tmin=-5,
-        tmax=30,
-        baseline=None,
-        preload=True
+    # Convert to haemoglobin and filter
+    raw_haemo = beer_lambert_law(raw_od, ppf=0.1)
+    raw_haemo = raw_haemo.filter(
+        0.02, 0.3, h_trans_bandwidth=0.1, l_trans_bandwidth=0.01, verbose=False
     )
 
-    return raw_filtre, epochs
+    # Apply further data cleaning techniques and extract epochs
+    raw_haemo = enhance_negative_correlation(raw_haemo)
+    # Extract events but ignore those with
+    # the word Ends (i.e. drop ExperimentEnds events)
+    events, event_dict = events_from_annotations(
+        raw_haemo
+        # raw_haemo, verbose=False, regexp="^(?![Ends]).*$"
+    )
+    epochs = Epochs(
+        raw_haemo,
+        events,
+        event_id=event_dict,
+        tmin=-5,
+        tmax=30,
+        reject=dict(hbo=200e-6),
+        reject_by_annotation=True,
+        proj=True,
+        baseline=(None, 0),
+        detrend=0,
+        preload=True,
+        verbose=False,
+    )
+    
 
-#%% fait sur tous les participants
+    return raw_haemo, epochs
+
+#%% Do the analysis for each participant
+#define the paths
+all_evokeds = defaultdict(list)
 
 path_root=cfg.rootpath
 path_data=os.path.join(path_root, "NIRS_DATA")
 path_raw=os.path.join(path_data, "NIRS_Raw_Data")
+#%%initiate variables
 files= glob.glob(os.path.join(path_raw, "*"))
 
-groupe_evokeds = defaultdict(list) #permet de memoriser les triggers
-for file in files:
-    print("\n>>> Traitement de:", file)
-    raw_filtre, epochs = individual_analysis(file)
+liste_epochs=[]
 
-    # Ajouter les evoked de ce participant au groupe
-for condition in epochs.event_id:
-    groupe_evokeds[condition].append(epochs[condition].average())
+for i,file in enumerate(files):
+    path= file
+    subject= file.split("_")[-1]
+    datatype= "nirs"
+    # Analyse data and return both ROI and channel results
+    raw_haemo, epochs = individual_analysis(path)
 
-#%% visualiser le décours temporel
+    # Save individual-evoked participant data along with others in all_evokeds
+    for cidx, condition in enumerate(epochs.event_id):
+        all_evokeds[condition].append(epochs[condition].average())
+        
+print(all_evokeds)
 
-#1 ligne avec 2 colonnes définit par groupe_evokes
-fig, axes = plt.subplots(nrows=1, ncols=len(groupe_evokeds), figsize=(17,5))
-lims = dict(hbo=[-5, 12], hbr=[-5, 12])
+
+#%%Visualisation
+#visualize Hbo and Hbr for each channels for easy and hard
+fig, axes = plt.subplots(nrows=1, ncols=len(all_evokeds), figsize=(17, 5))
+lims = dict(hbo=[-5, 20], hbr=[-5, 20])
 
 for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
-    for idx, evoked in enumerate(groupe_evokeds):
+    for idx, evoked in enumerate(all_evokeds):
         plot_compare_evokeds(
-            {evoked: groupe_evokeds[evoked]},
-            combine="mean", 
+            {evoked: all_evokeds[evoked]},
+            #combine="mean",
             picks=pick,
-            axes=axes[idx], #evoked
+            axes=axes[idx],
             show=False,
             colors=[color],
             legend=False,
             ylim=lims,
-            ci=0.95,  #interval de confiance 
+            ci=0.95,
             show_sensors=idx == 2,
         )
         axes[idx].set_title(f"{evoked}")
 axes[0].legend(["Oxyhaemoglobin", "Deoxyhaemoglobin"])
+#%% Visualisation for each ROI
+#First let's define our 3 ROI: left, center and right based on the montage
+left = [
+    [2,1], [2,3], #source S2 
+    [1,1], [1,2], #source S1 
+    [3,3], [3,2], #source S3 
+]
 
-#%% sur les régions d'intéret 
+center = [
+    [4,2], [4,5], #source S4 
+    [5,3], [5,6], #source S5
+]
 
-# Specify channel pairs for each ROI
-left = [[1,1], [2,1], [2,2], [3,2]]
-center = [[3, 3], [5,4], [4, 4]]
-right = [[5,5], [6,6], [7,6], [8,7]]
-
-def picks_pair_to_idx(raw, pairs):
-    idxs = []
-    for s, d in pairs:
-        for ch_idx, ch_name in enumerate(raw.info['ch_names']):
-            if f"S{s}_D{d}" in ch_name:
-                idxs.append(ch_idx)
-    return idxs
-
+right = [
+    [6,5], [6,6],
+    [7,5], [7,7],
+    [8,6], [8,7],
+]
 # Then generate the correct indices for each pair and store in dictionary
 rois = dict(
-    Left_Hemisphere=picks_pair_to_idx(raw_filtre, left),
-    Center=picks_pair_to_idx(raw_filtre, center),
-    Right_Hemisphere=picks_pair_to_idx(raw_filtre, right),
+    Left_Hemisphere=picks_pair_to_idx(raw_haemo, left),
+    Center=picks_pair_to_idx(raw_haemo, center),
+    Right_Hemisphere=picks_pair_to_idx(raw_haemo, right),
 )
 
-pprint(rois)
+print(rois)
 
-#%% graph par rois 
-
-# Specify the figure size and limits per chromophore.
-fig, axes = plt.subplots(nrows=len(rois), ncols=len(groupe_evokeds), figsize=(15, 6))
+#Visualisation
+fig, axes = plt.subplots(nrows=len(rois), ncols=len(all_evokeds), figsize=(15, 6))
 lims = dict(hbo=[-8, 16], hbr=[-8, 16])
 
 for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
     for ridx, roi in enumerate(rois):
-        for cidx, evoked in enumerate(groupe_evokeds):
+        for cidx, evoked in enumerate(all_evokeds):
             if pick == "hbr":
                 picks = rois[roi][1::2]  # Select only the hbr channels
             else:
                 picks = rois[roi][0::2]  # Select only the hbo channels
 
             plot_compare_evokeds(
-                {evoked: groupe_evokeds[evoked]},
+                {evoked: all_evokeds[evoked]},
                 combine="mean",
                 picks=picks,
                 axes=axes[ridx, cidx],
@@ -147,23 +198,23 @@ for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
                 ci=0.95,
                 show_sensors=cidx == 2,
             )
-            axes[ridx, cidx].set_title([evoked])
+            axes[ridx, cidx].set_title("")
         axes[0, cidx].set_title(f"{evoked}")
         axes[ridx, 0].set_ylabel(f"{roi}\nChromophore (ΔμMol)")
 axes[0, 0].legend(["Oxyhaemoglobin", "Deoxyhaemoglobin"])
 
-#%% amplitude 6s post-stimulus 
+#%%Visualisation of maximal amplitudes for each subject
 
-df = pd.DataFrame(columns=["ID", "ROI", "Chroma", "Condition", "Value"])
-subj_id = 0
-for idx, evoked in enumerate(groupe_evokeds):
-    
-    for subj_data in groupe_evokeds[evoked]:
+df = pd.DataFrame(columns=["ID", "ROI", "Chroma", "Condition", "Amplitude"])
+
+for idx, evoked in enumerate(all_evokeds):
+    subj_id = 0
+    for subj_data in all_evokeds[evoked]:
         subj_id += 1
         for roi in rois:
             for chroma in ["hbo", "hbr"]:
                 data = deepcopy(subj_data).pick(picks=rois[roi]).pick(chroma)
-                value = data.crop(tmin=5.0, tmax=7.0).data.max() * 1.0e6 #prend 6s & amplitude max
+                value = data.crop(tmin=5.0, tmax=7.0).data.mean() * 1.0e6
 
                 # Append metadata and extracted feature to dataframe
                 this_df = pd.DataFrame(
@@ -177,41 +228,9 @@ for idx, evoked in enumerate(groupe_evokeds):
                     index=[0],
                 )
                 df = pd.concat([df, this_df], ignore_index=True)
-                
+
 df.reset_index(inplace=True, drop=True)
-#df["Value"] = pd.to_numeric(df["Value"])  # some Pandas have this as object
+df["Value"] = pd.to_numeric(df["Value"])
 
+#%%Visualise max amplitudes for each participant for Hbo and Hbr
 
-
-#%%
-
-
-
-#%% visualise en graph 
-
-
-
-#Hbo
-sns.catplot(
-    x="Condition",
-    y="Value",
-    hue="ID",
-    data=df.query("Chroma == 'hbo'"),
-    errorbar=None,
-    palette="muted",
-    height=4,
-    s=10,
-)
-
-#hbr
-
-sns.catplot(
-    x="Condition",
-    y="Value",
-    hue="ID",
-    data=df.query("Chroma == 'hbr'"),
-    errorbar=None,
-    palette="muted",
-    height=4,
-    s=10,
-)
