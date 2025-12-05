bc93ba1e91ba757c5d0ca3f779b75ccf17b2656c
Aliceee275 <gaut.alice@free.fr>
2025-12-05 01:04:12 +0100

    Update sanstitre1.py

--- sanstitre1.py
+++ sanstitre1.py
@@ -3,26 +3,37 @@
 Created on Fri Nov 28 23:35:19 2025
 
 @author: gauta
+
+In this script, we analyze the Nirs data of 4 participants solving hard 
+and easy math questions. 
+
+Our goal is to determine which ROI is the most sensitive to difficulty.
+
+This script is based on our classes, the plot_16_waveform_group.ipynb,
+and follows the instruction of our project found on moodle.
+
+The mne and mne_nirs, as bell as graph related libraries are required.
+A config script containing the paths is also necessary, with the 
+necessary changes to work on your computer.
 """
 
-#%% Import librairiesimport os
-import config as cfg
+#%% Import librairies
+#standard imports
 import glob
 import os
 
-# Import common libraries
 from collections import defaultdict
 from copy import deepcopy
 from itertools import compress
-from pprint import pprint
 
-# Import Plotting Library
+# plotting and statistic analysis libraries
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-
-# Import StatsModels
 import statsmodels.formula.api as smf
+
+#non standard imports
+import mne
 from mne import Epochs, events_from_annotations, set_log_level
 from mne.preprocessing.nirs import (
     beer_lambert_law,
@@ -30,58 +41,107 @@ from mne.preprocessing.nirs import (
     scalp_coupling_index,
     temporal_derivative_distribution_repair,
 )
-
-# Import MNE processing
 from mne.viz import plot_compare_evokeds
 
-# Import MNE-BIDS processing
-import mne
-
-# Import MNE-NIRS processing
 from mne_nirs.channels import get_long_channels, picks_pair_to_idx
-from mne_nirs.datasets import fnirs_motor_group
 from mne_nirs.signal_enhancement import enhance_negative_correlation
 
+#local imports
+import config as cfg
+
 # Set general parameters
-set_log_level("WARNING")  # Don't show info, as it is repetitive for many subjects
+# Don't show info, as it is repetitive for many subjects
+set_log_level("WARNING")  
+
+#%%
+"""
+
+We follow the step as seen in class, and add the steps from the 
+plot_16_waveform_group.ipynb and the project on Moodle:
+    
+1- Load the data (Nirx Format)
+1bis - Rename trigger 1 and 2 in Hard and Easy
+
+2- Convert intensity to OD
+2bis - Downsample to 2Hz
+
+3- Artifact correction (TDDR)
+
+4- Convert OD to concentration
+
+5- Filtering with pass band ([0.002 - 0.3] Hz)
+
+6- Create epochs [-5; +30] ms around the events
+
+7- Trial averaging (not in the individual_analysis function)
+
+"""
 
 #%%define the analysis function
 
-def individual_analysis(path):
-    # load the Nirx data
-    raw_intensity= mne.io.read_raw_nirx(path, verbose=False)
-    raw_intensity = get_long_channels(raw_intensity, min_dist=0.01)
-    
-    #rename trigger 1 and 2 into 'Hard' and 'Easy'
-    #events,event_ids= mne.events_from_annotations(raw_intensity)
-    raw_intensity.annotations.rename({'1.0': 'Hard', '2.0':'Easy'})
 
-    # Convert signal to optical density and determine bad channels
+def individual_analysis(path):
+    # 1- Load the data (Nirx Format)
+    raw_intensity= mne.io.read_raw_nirx(
+        path,
+        verbose=False
+        )
+    raw_intensity = get_long_channels(
+        raw_intensity,
+        min_dist=0.01
+        )
+    
+    # 1bis - Rename trigger 1 and 2 in Hard and Easy
+    raw_intensity.annotations.rename({'1.0': 'Hard', '2.0':'Easy'})
+    #visualize (optional, for the presentation)
+    #raw_intensity.plot(duration=30)
+    
+    # 2- Convert intensity to OD and determine bad channels
     raw_od = optical_density(raw_intensity)
-    sci = scalp_coupling_index(raw_od, h_freq=1.35, h_trans_bandwidth=0.1)
+    sci = scalp_coupling_index(
+        raw_od,
+        h_freq=1.35,
+        h_trans_bandwidth=0.1
+        )
     raw_od.info["bads"] = list(compress(raw_od.ch_names, sci < 0.5))
     raw_od.interpolate_bads()
-
-    # Down sample to 2 Hz
-    raw_od.resample(2)
+    #visualize
+    #raw_od.plot(duration=30)
     
-    #apply movement correction (TDDR)
+    # 2bis- Down sample to 2 Hz
+    raw_od.resample(2)
+    #visualize
+    #raw_od.plot(duration=30)
+    
+    #3 - Artifact correction : apply movement correction (TDDR)
     raw_od = temporal_derivative_distribution_repair(raw_od)
+    
+    #visualize
+    #raw_od.plot(duration=30)
 
-    # Convert to haemoglobin and filter
-    raw_haemo = beer_lambert_law(raw_od, ppf=0.1)
+    # 4- Convert OD to concentration (haemoglobin) 
+    raw_haemo = beer_lambert_law(
+        raw_od,
+        ppf=0.1
+        )
+    # 5- Filtering with pass band ([0.002 - 0.3] Hz)
     raw_haemo = raw_haemo.filter(
-        0.02, 0.3, h_trans_bandwidth=0.1, l_trans_bandwidth=0.01, verbose=False
+        0.02,
+        0.3,
+        h_trans_bandwidth=0.1,
+        l_trans_bandwidth=0.01,
+        verbose=False
     )
+    #visualize
+    #raw_haemo.plot(duration=30)
 
-    # Apply further data cleaning techniques and extract epochs
+    # Apply further data cleaning techniques (mne tutorial) and extract epochs
     raw_haemo = enhance_negative_correlation(raw_haemo)
-    # Extract events but ignore those with
-    # the word Ends (i.e. drop ExperimentEnds events)
+    # Extract events
     events, event_dict = events_from_annotations(
         raw_haemo
-        # raw_haemo, verbose=False, regexp="^(?![Ends]).*$"
-    )
+        )
+    #Create epochs
     epochs = Epochs(
         raw_haemo,
         events,
@@ -97,26 +157,26 @@ def individual_analysis(path):
         verbose=False,
     )
     
-
     return raw_haemo, epochs
 
-#%% Do the analysis for each participant
-#define the paths
-all_evokeds = defaultdict(list)
 
+#%% Do the analysis for each participant
+#define the paths of the Nirx files
 path_root=cfg.rootpath
 path_data=os.path.join(path_root, "NIRS_DATA")
 path_raw=os.path.join(path_data, "NIRS_Raw_Data")
-#%%initiate variables
+
 files= glob.glob(os.path.join(path_raw, "*"))
 
-liste_epochs=[]
+#Initiate a dictionnary to store the evokeds for each condition
+all_evokeds = defaultdict(list)
 
+#run the analysis for each participant
 for i,file in enumerate(files):
     path= file
     subject= file.split("_")[-1]
     datatype= "nirs"
-    # Analyse data and return both ROI and channel results
+    # extract the epochs and raw_haemo for each participant
     raw_haemo, epochs = individual_analysis(path)
 
     # Save individual-evoked participant data along with others in all_evokeds
@@ -125,17 +185,40 @@ for i,file in enumerate(files):
         
 print(all_evokeds)
 
+#%% 
+"""
 
-#%%Visualisation
-#visualize Hbo and Hbr for each channels for easy and hard
-fig, axes = plt.subplots(nrows=1, ncols=len(all_evokeds), figsize=(17, 5))
-lims = dict(hbo=[-5, 20], hbr=[-5, 20])
+Then we move on to the analysis part:
 
+First, we visualize the temporal discourse of hbO and hHb,
+with a mean of all channels for both conditions
+
+Then, we visualize the same thing but for out 3 ROIs: left, center and right
+
+Finally we compute the max amplitude for each participant for bhO and hHb 
+
+The goal is to determine which ROI is the most affected 
+by the effort difference between Hard and Easy
+
+Therefore, for each ROI we compute the mean difference between conditions 
+for all participants and visualize this
+
+"""
+
+#%%Visualisation of Hbo and Hbr for each channels for easy and hard
+fig, axes = plt.subplots(nrows=1, ncols=len(all_evokeds), figsize=(15, 8))
+
+#To have the same scale everywhere, we set the limits of the y axis
+lims = dict(hbo=[-10, 20], hbr=[-10, 20])
+
+#For both hbo and hbr(=hHb)
 for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
+    #For each condition of evoked (all the epochs in easy and all the epochs in hard)
     for idx, evoked in enumerate(all_evokeds):
+        #We use the mne function to visualize both evokeds (hbo and hbr)
         plot_compare_evokeds(
             {evoked: all_evokeds[evoked]},
-            #combine="mean",
+            combine="mean",
             picks=pick,
             axes=axes[idx],
             show=False,
@@ -146,6 +229,7 @@ for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
             show_sensors=idx == 2,
         )
         axes[idx].set_title(f"{evoked}")
+        
 axes[0].legend(["Oxyhaemoglobin", "Deoxyhaemoglobin"])
 #%% Visualisation for each ROI
 #First let's define our 3 ROI: left, center and right based on the montage
@@ -161,11 +245,12 @@ center = [
 ]
 
 right = [
-    [6,5], [6,6],
-    [7,5], [7,7],
-    [8,6], [8,7],
+    [6,5], [6,6], #source S6
+    [7,5], [7,7], #source S7
+    [8,6], [8,7], #source S8
 ]
-# Then generate the correct indices for each pair and store in dictionary
+
+# Then generate the correct indices for each pair and store in dictionary rois
 rois = dict(
     Left_Hemisphere=picks_pair_to_idx(raw_haemo, left),
     Center=picks_pair_to_idx(raw_haemo, center),
@@ -175,8 +260,8 @@ rois = dict(
 print(rois)
 
 #Visualisation
-fig, axes = plt.subplots(nrows=len(rois), ncols=len(all_evokeds), figsize=(15, 6))
-lims = dict(hbo=[-5, 20], hbr=[-5, 20])
+fig, axes = plt.subplots(nrows=len(rois), ncols=len(all_evokeds), figsize=(12, 10))
+lims = dict(hbo=[-10, 20], hbr=[-10, 20])
 
 for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
     for ridx, roi in enumerate(rois):
@@ -201,10 +286,11 @@ for pick, color in zip(["hbo", "hbr"], ["r", "b"]):
             axes[ridx, cidx].set_title("")
         axes[0, cidx].set_title(f"{evoked}")
         axes[ridx, 0].set_ylabel(f"{roi}\nChromophore (ΔμMol)")
+        
 axes[0, 0].legend(["Oxyhaemoglobin", "Deoxyhaemoglobin"])
 
 #%%Visualisation of maximal amplitudes for each subject
-
+#First we create a df and compute the maximal amplitudes
 df = pd.DataFrame(columns=["ID", "ROI", "Chroma", "Condition", "Amplitude"])
 
 for idx, evoked in enumerate(all_evokeds):
@@ -214,6 +300,7 @@ for idx, evoked in enumerate(all_evokeds):
         for roi in rois:
             for chroma in ["hbo", "hbr"]:
                 data = deepcopy(subj_data).pick(picks=rois[roi]).pick(chroma)
+                #We get the maximal amplitude value around the supposed peak (at 6 sec post stimulus) 
                 value = data.crop(tmin=5.0, tmax=7.0).data.max() * 1.0e6
 
                 # Append metadata and extracted feature to dataframe
@@ -227,13 +314,14 @@ for idx, evoked in enumerate(all_evokeds):
                     },
                     index=[0],
                 )
+                #We add this_df to the pre existing df 
                 df = pd.concat([df, this_df], ignore_index=True)
 
 df.reset_index(inplace=True, drop=True)
 df["Value"] = pd.to_numeric(df["Value"])
 
-#%%Visualise max amplitudes for each participant for Hbo and Hbr
-
+#%%Visualize max amplitudes for each participant for Hbo and Hbr
+#Allows to visualize
 for roi in rois:
     df_roi= df.loc[df['ROI']==roi]
     sns.catplot(
@@ -246,33 +334,41 @@ for roi in rois:
         height=4,
         s=10,
     )
-#%% en lineplot
+    
+#%% Visualize max amplitudes for each participant for Hbo and Hbr with a lineplot 
+chromas= ['hbo', 'hbr']
+
+#First we set the limits of the y axis to have the same scale for each figure
 y_min = df.loc[df['Chroma']=='hbo', 'Value'].min()
 y_max = df.loc[df['Chroma']=='hbo', 'Value'].max()+1
 
-fig, axes = plt.subplots(nrows=1, ncols=len(rois), figsize=(17, 5))
+#To have all figures in the same window: subplot for each ROI and chroma
+fig, axes = plt.subplots(nrows=len(chromas), ncols=len(rois), figsize=(14, 10))
 
-for idx, roi in enumerate(rois):
-    df_roi = df.loc[df['ROI']==roi].query("Chroma == 'hbo'")
-    
-    # Line plot to see the difference for a same participant
-    sns.lineplot(
-        x="Condition", 
-        y="Value",
-        hue="ID",
-        data=df_roi,
-        palette="muted",
-        marker='o',
-        ax=axes[idx],  # Utiliser l'index
-        legend=True,
-    )
-    axes[idx].set_ylim(y_min, y_max) 
-    axes[idx].set_title(f"ROI: {roi}")  # Appliquer le titre à l'axe spécifique
+for row , chrom in enumerate(chromas):
+    for col, roi in enumerate(rois):
+        df_roi = df.loc[df['ROI']==roi].query("Chroma == @chrom")
+        ax = axes[row, col]
+        sns.lineplot(
+            x="Condition", 
+            y="Value",
+            hue="ID",
+            data=df_roi,
+            palette="muted",
+            marker='o',
+            ax=ax, 
+            legend=True,
+        )
+        ax.set_ylim(y_min, y_max) 
+        #put the titles for each column and row
+        ax.set_title(f"ROI: {roi}") 
+        ax.set_ylabel(chrom)
 
-plt.tight_layout()  # Pour mieux espacer les subplots
+plt.tight_layout()  
 plt.show()
 
-#%%
+#%%Visualize amplitude difference: hard/easy conditions for each participant
+
 dict_delta = {r: [] for r in rois}
 for roi in df.ROI.unique():
     df_roi = df.loc[df['ROI']==roi].query("Chroma == 'hbo'")
@@ -284,79 +380,59 @@ for roi in df.ROI.unique():
         delta= hard-easy
         dict_delta[roi].append(delta)
         print(df_roi.Chroma)
-#%%
+        
 df_delta = pd.DataFrame.from_dict(dict_delta)
 
-#%%
-plt.figure(figsize=(10, 6))
+# Visualize
+fig, axes = plt.subplots(nrows=len(chromas), ncols=1, figsize=(7, 10))
 
-# Dégradé de violettes
-pastel_colors = ["#D5E8D4",  # Vert forêt clair
-                   "#66BB6A",  # Vert forêt moyen  
-                   "#3D8B3D"]
+# we create a color palette for our ROIs to make the boxplot more visual
+colors = ["#D5E8D4", "#66BB6A", "#3D8B3D"]
+#Then we make a boxplot and a stripplot to see the data of each individual
 
-sns.boxplot(data=df_delta, palette=pastel_colors)
+for row, chrom in enumerate(chromas):
+    ax = axes[row]
+    sns.boxplot(data=df_delta, palette=colors, ax=ax)
+    sns.stripplot(
+        data=df_delta,
+        palette=colors,
+        alpha=0.7,
+        size=6,
+        edgecolor='gray',
+        linewidth=0.5, 
+        dodge=True, 
+        ax=ax
+        )
+    #ax.set_ylim(y_min, y_max) 
+    ax.set_ylabel(chrom)
+    ax.set_title(f'Amplitude difference for each ROI ({chroma})')
 
-# Stripplot avec la même palette pour éviter les doublons
-sns.stripplot(data=df_delta, palette=pastel_colors, alpha=0.7, size=6, edgecolor='gray', linewidth=0.5, dodge=True)
 
-plt.title('Boxplot with Individual Data Points')
-plt.ylabel('Delta Values')
+plt.tight_layout()
 plt.show()
 
+#print the mean for each ROI
 for col in df_delta.columns:
-    print('moyenne:', df_delta[col].mean(), '+/-', df_delta[col].std())
+    print('mean +/- std:', df_delta[col].mean(), '+/-', df_delta[col].std())
 
-#%% same but with effect size
-import numpy as np 
-from math import sqrt
-dict_effect = {r: {'hard': [], 'easy': []} for r in rois}
-
-for roi in df.ROI.unique():
-    df_roi = df.loc[df['ROI']==roi].query("Chroma == 'hbo'")
-    print(roi)
-    for sub in df_roi['ID'].unique():
-        easy= df_roi.loc[df_roi.ID == sub].query("Condition == 'Easy'")['Value'].values[0]
-        hard= df_roi.loc[df_roi.ID == sub].query("Condition == 'Hard'")['Value'].values[0]
-        dict_effect[roi]['easy'].append(easy)
-        dict_effect[roi]['hard'].append(hard)
-
-
-#%%
-df_delta = pd.DataFrame.from_dict(dict_delta)
-
-#%%
-plt.figure(figsize=(10, 6))
-
-# Dégradé de violettes
-pastel_colors = ["#D5E8D4",  # Vert forêt clair
-                   "#66BB6A",  # Vert forêt moyen  
-                   "#3D8B3D"]
-
-sns.boxplot(data=df_delta, palette=pastel_colors)
-
-# Stripplot avec la même palette pour éviter les doublons
-sns.stripplot(data=df_delta, palette=pastel_colors, alpha=0.7, size=6, edgecolor='gray', linewidth=0.5, dodge=True)
-
-plt.title('Boxplot with Individual Data Points')
-plt.ylabel('Delta Values')
-plt.show()
-
-for col in df_delta.columns:
-    print('moyenne:', df_delta[col].mean(), '+/-', df_delta[col].std())
-    
-#%% LMM
+#%% For statistical analysis, we use a LMM (effects of ROI and conditions)
 input_data = df.query("Condition in ['Hard', 'Easy']")
 input_data = input_data.query("Chroma in ['hbo']")
-# input_data = input_data.query("ROI in ['Center','Right_Hemisphere','Left_Hemisphere']")
+
+#We have to put the ROIs as categorical variables for the LMM
 input_data["ROI"] = pd.Categorical(
     input_data["ROI"],
     categories=["Center", "Left_Hemisphere", "Right_Hemisphere"]
 )
+
+#Reordering data allows to change the reference for the LMM
 input_data["ROI"] = input_data["ROI"].cat.reorder_categories(
     ["Left_Hemisphere", "Center", "Right_Hemisphere"],
     ordered=False
 )
+
+#we create our model
 roi_model = smf.mixedlm("Value ~ Condition*ROI", input_data, groups=input_data["ID"]).fit()
 
+#we print the summary with the statistical data
 roi_model.summary()
